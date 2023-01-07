0692428ecb40370c6da1b2c1891e4f2b08e477eb
AmmarKhawaja <ammarkhawaja01@gmail.com>
2023-01-06 20:56:23 -0500

    Made changes to output - Textbox is smaller and higher up. - Background video is now randomized. - Fixed bug in bad word detection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from moviepy.editor import *
 from gtts import *
 import secret as s
+import random
 #change_settings({"IMAGEMAGICK_BINARY": "./ImageMagick-7.0.10"})
 import praw
 if __name__ == '__main__':
@@ -8,20 +9,23 @@ if __name__ == '__main__':
     reddit = praw.Reddit(client_id=s.CLIENT_ID,
                          client_secret=s.CLIENT_SECRET,
                          username=s.USERNAME, password=s.PASSWORD,
-                         user_agent="bismillah")
+                         user_agent="ammarkhawaja")
     subreddit = reddit.subreddit('askreddit').hot(limit=10)
     for post in subreddit:
         if not post.stickied:
             bad_post = False
             reddit_post = post.title
+            print(reddit_post)
             reddit_comments = post.comments
             for word in s.BLACKLISTED_WORDS:
-                if word in reddit_post or len(reddit_post) > 50 or len(post.comments[0].body) > 300 or len(post.comments[1].body) > 300:
+                if word in reddit_post or word in post.comments[0].body or word in post.comments[1].body or len(reddit_post) > 100 or len(post.comments[0].body) > 200 or len(post.comments[1].body) > 200:
                     bad_post = True
             if not bad_post:
                 break
 
-    file_title = "content/" + post.title + " #askreddit #reddit .mp4"
+    post.title = post.title.replace("/", " or ")
+
+    file_title = r"content/" + post.title + " #askreddit #reddit .mp4"
 
     print("Audio Preparing")
     audio_references = ["content/title.mp3", "content/comment0.mp3", "content/comment1.mp3", "content/fullaudio.mp3"]
@@ -39,11 +43,11 @@ if __name__ == '__main__':
     print("Audio Ready")
 
     print("Text Preparing")
-    video = VideoFileClip("content/backgroundmovie.mov").resize((1080, 1920))
-    background_image = ImageClip("content/background.png").set_start(0).set_duration(video.duration).set_pos(("center","center")).resize(1.2,1.2)
-    title_text = TextClip(txt=reddit_post, fontsize=32, align="west",size=(700,300), color="white", method="caption").set_position("center").set_duration(AudioFileClip(audio_references[0]).duration)
-    comment0_text = TextClip(txt=reddit_comments[0].body, align="west", fontsize=26, size=(700,300), color="white", method="caption").set_position("center").set_duration(AudioFileClip(audio_references[1]).duration)
-    comment1_text = TextClip(txt=reddit_comments[1].body, align="west", fontsize=26, size=(700,300), color="white", method="caption").set_position("center").set_duration(AudioFileClip(audio_references[2]).duration)
+    video = VideoFileClip("content/backgroundmovie.mp4").resize((1080, 1920)).subclip(random.randrange(360), 480)
+    background_image = ImageClip("content/background.png").set_start(0).set_duration(video.duration).set_pos(("center", 500)).resize(1.1,1.1)
+    title_text = TextClip(txt=reddit_post, fontsize=32, align="west",size=(650,300), color="white", method="caption").set_position(("center", 600)).set_duration(AudioFileClip(audio_references[0]).duration)
+    comment0_text = TextClip(txt=reddit_comments[0].body, align="west", fontsize=26, size=(650,300), color="white", method="caption").set_position(("center", 600)).set_duration(AudioFileClip(audio_references[1]).duration)
+    comment1_text = TextClip(txt=reddit_comments[1].body, align="west", fontsize=26, size=(650,300), color="white", method="caption").set_position(("center", 600)).set_duration(AudioFileClip(audio_references[2]).duration)
 
     title_text.save_frame("content/title_text.png")
     comment0_text.save_frame("content/comment0_text.png")
